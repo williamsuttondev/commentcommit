1528d3ddc0d65df0282d25cc6b56658bb7d039da
William Sutton <psyws4@nottingham.ac.uk>
2023-01-08 02:28:09 +0000

    Refactored - Changed the return and handling methods of Configs struct
    Additionally, added the Path crate to detect a file, small issue being present that the file is not being detected if it does exist.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod read;
 
 use std::env;
-use std::fs;
 use read::Config;
 
 fn main() {
-    let cfg: Config = Config::create(env::args());
-    match cfg.get_location().as_str() {
-        read::SHORT_ERROR => println!("There were too few arguments, please enter more arguments. (Error code {})", read::SHORT_ERROR),
-        _ => read::read_file(),
+    let cfg = match Config::create(env::args()) {
+        Ok(configuration) => configuration,
+        Err(e) => panic!("{}", e),
+    };
+    if read::check_valid(&cfg.location) {
+        println!("The file exists!");
+    } else {
+        println!("The file does not exist!");
     }
 }
--- src/read.rs
+++ src/read.rs
@@ -1,17 +1,14 @@
 use std::env::Args;
-
+//use std::fs;
+use std::path::Path;
 
 const EXPECTED_LENGTH_OF_ARGS:u8 = 2;
-pub const SHORT_ERROR:&str = "0X0";
 pub struct Config {
-    location: String
+    pub location: String
 }
 
 impl Config {
-    pub fn get_location(&self) -> &String {
-        &self.location
-    }
-    pub fn create(args : Args) -> Config {
+    pub fn create(args : Args) -> Result<Config, &'static str> {
         let mut parsed:Vec<String> = Vec::new();
         let mut length:u8 = 0;
         for arg in args {
@@ -19,18 +16,15 @@ impl Config {
             length+=1;
             if length == EXPECTED_LENGTH_OF_ARGS {
                 let location:String=parsed[1].clone();
-                return Config {
-                    location
-                };
+                return Ok(Config {
+                    location,
+                })
             }
         }
-        let location: String = SHORT_ERROR.to_string();
-        return Config {
-            location,
-        }
+        return Err("There are too few arguments in the command line, please try again.");
     }
 }
 
-pub fn read_file() {
-    
+pub fn check_valid(path : &String) -> bool {
+    Path::new(path).exists()
 }
