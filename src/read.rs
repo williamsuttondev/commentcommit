0d8501932f70e3b6a4c40ed1e07737d304d88ae5
William Sutton <psyws4@nottingham.ac.uk>
2023-01-09 00:04:58 +0000

    Feature - The command parser now works successfully
    Need to implement the command type to enum types and add error checking

--- src/read.rs
+++ src/read.rs
@@ -3,8 +3,26 @@ use std::fs;
 use std::io;
 
 const EXPECTED_LENGTH_OF_ARGS:u8 = 2;
+
+enum CommandType {
+    C1,
+    C2,
+    C3,
+    C4,
+    C5,
+    C6,
+    C7,
+    C8,
+    C9,
+    C10
+}
+
 pub struct Config {
     pub location: String
+}
+pub struct Command {
+    command_type: CommandType,
+    queury_content: String
 }
 
 impl Config {
@@ -27,15 +45,33 @@ impl Config {
 pub fn get_contents(path: &String) -> io::Result<String> {
     fs::read_to_string(path)
 }
-pub fn split_by_comment(content: String) {
+pub fn split_by_comment(content: String){
     let line = content.lines();
+    let mut cleansed:Vec<String> = Vec::new();
     for indv in line {
         let handle = indv.trim();
         if handle.starts_with("###") {
             let linespl:Vec<&str> = handle.split("###").collect();
-            println!("I am comment!: {}", linespl[1].trim());
-        } else {
-            println!("I am not a comment {}", indv);
+            cleansed.push(linespl[1].trim().to_string());
         }
     }
+    println!("{:?}", cleansed);
+    if !commands(cleansed) {
+        panic!("There was an error executing one of the commands, please make sure you used valid syntax.");
+    }
+}
+
+fn commands(mut command_arr: Vec<String>) -> bool {
+    let contents_action = command_arr.iter_mut();
+    for actionable_line in contents_action {
+        if actionable_line.starts_with("@") {
+            actionable_line.remove(0);
+            let delimited:(&str, &str) = match actionable_line.split_once(" ") {
+                Some(x) => x,
+                None => panic!("You need to enter a command after the @!"),
+            };
+            println!("Command: {}, Query: {}",delimited.0, delimited.1);
+        }
+    }
+    true
 }
