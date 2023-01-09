927d06b85e5824aadcca2dc5db17c863dc6292b4
William Sutton <psyws4@nottingham.ac.uk>
2023-01-09 00:32:36 +0000

    Impl - Command type vector now returned of all commands

--- src/read.rs
+++ src/read.rs
@@ -1,10 +1,13 @@
 use std::env::Args;
 use std::fs;
 use std::io;
+use std::process;
 
 const EXPECTED_LENGTH_OF_ARGS:u8 = 2;
 
+#[derive(Debug)]
 enum CommandType {
+    C0,
     C1,
     C2,
     C3,
@@ -13,16 +16,16 @@ enum CommandType {
     C6,
     C7,
     C8,
-    C9,
-    C10
+    C9
 }
 
 pub struct Config {
     pub location: String
 }
+#[derive(Debug)]
 pub struct Command {
     command_type: CommandType,
-    queury_content: String
+    query_content: String
 }
 
 impl Config {
@@ -56,22 +59,34 @@ pub fn split_by_comment(content: String){
         }
     }
     println!("{:?}", cleansed);
-    if !commands(cleansed) {
-        panic!("There was an error executing one of the commands, please make sure you used valid syntax.");
+    match commands(cleansed) {
+        Ok(x) => println!("{:?}", x),
+        Err(e) => {println!("The command interpreter failed due to: {}", e); process::exit(1);},
     }
 }
 
-fn commands(mut command_arr: Vec<String>) -> bool {
+fn commands(mut command_arr: Vec<String>) -> Result<Vec<Command>, &'static str> {
+    let mut commands_and_contents:Vec<Command> = Vec::new();
+    let mut accepted:bool = true;
     let contents_action = command_arr.iter_mut();
     for actionable_line in contents_action {
         if actionable_line.starts_with("@") {
             actionable_line.remove(0);
             let delimited:(&str, &str) = match actionable_line.split_once(" ") {
                 Some(x) => x,
-                None => panic!("You need to enter a command after the @!"),
+                None => {accepted = !accepted; break},
             };
-            println!("Command: {}, Query: {}",delimited.0, delimited.1);
+            let ccentry: Command = Command {
+                command_type: CommandType::C0,
+                query_content: delimited.1.to_string(),
+            };
+            commands_and_contents.push(ccentry);
         }
     }
-    true
+    if accepted {
+        Ok(commands_and_contents)
+    }
+    else {
+        Err("There was an error parsing the commands!")
+    }
 }
