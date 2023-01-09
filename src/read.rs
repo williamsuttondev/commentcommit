df98eaee0d31b2c5af1fa842ce5c659d46fbefb6
William Sutton <psyws4@nottingham.ac.uk>
2023-01-09 03:50:03 +0000

    Refactor - Refactored code to be more readable

--- src/read.rs
+++ src/read.rs
@@ -32,82 +32,77 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn create(args : Args) -> Result<Config, &'static str> {
-        let mut parsed:Vec<String> = Vec::new();
-        let mut length:u8 = 0;
+    pub fn create(args: Args) -> Result<Config, &'static str> {
+        let mut parsed: Vec<String> = Vec::new();
+        let expected_length = EXPECTED_LENGTH_OF_ARGS;
+        let error_msg = "There are too few arguments in the command line, please try again.";
         for arg in args {
             parsed.push(arg);
-            length+=1;
-            if length == EXPECTED_LENGTH_OF_ARGS {
-                let location:String=parsed[1].clone();
-                return Ok(Config {
-                    location,
-                })
+            if parsed.len() == expected_length as usize {
+                let location = &parsed[1];
+                return Ok(Config { location: location.to_string() });
             }
         }
-        return Err("There are too few arguments in the command line, please try again.");
+        Err(error_msg)
     }
 }
 pub fn get_contents(path: &String) -> io::Result<String> {
     fs::read_to_string(path)
 }
-pub fn split_by_comment(content: String){
-    let line = content.lines();
-    let mut cleansed:Vec<String> = Vec::new();
-    for indv in line {
-        let handle = indv.trim();
-        if handle.starts_with("###") {
-            let linespl:Vec<&str> = handle.split("###").collect();
-            cleansed.push(linespl[1].trim().to_string());
-        }
+pub fn split_by_comment(content: String) {
+    let mut cleansed: Vec<String> = Vec::new();
+    for indv in content.split("###") {
+        cleansed.push(indv.trim_start_matches("###").trim().to_string());
     }
-    println!("{:?}", cleansed);
-    match commands(cleansed) {
-        Ok(x) => println!("{:?}", x),
-        Err(e) => {println!("The command interpreter failed due to: {}", e); process::exit(1);},
-    }
+    let x = commands(cleansed).unwrap_or_else(|e| {
+        println!("The command interpreter failed due to: {}", e);
+        process::exit(1);
+    });
+    println!("{:?}", print_out(&x));
 }
 
 fn commands(mut command_arr: Vec<String>) -> Result<Vec<Command>, &'static str> {
     let mut commands_and_contents:Vec<Command> = Vec::new();
-    let mut accepted:bool = true;
-    let contents_action = command_arr.iter_mut();
-    for actionable_line in contents_action {
-        if actionable_line.starts_with("@") {
-            actionable_line.remove(0);
-            let delimited:(&str, &str) = match actionable_line.split_once(" ") {
-                Some(x) => x,
-                None => {accepted = !accepted; break},
-            };
-            let ccentry: Command = Command {
-                command_type: match commandtype(delimited.0) {
-                    Ok(ct) => ct,
-                    Err(e) => panic!("Something went wrong with the command type!: {}", e),
-                },
-                query_content: delimited.1.to_string(),
-            };
-            commands_and_contents.push(ccentry);
+    for actionable_line in command_arr.iter_mut() {
+        if !actionable_line.starts_with("@") {
+            continue;
         }
+        actionable_line.remove(0);
+        let parts:Vec<&str> = actionable_line.splitn(2, ' ').collect();
+        let ccentry: Command = Command {
+            command_type: match commandtype(parts[0]) {
+                Ok(ct) => ct,
+                Err(e) => panic!("Something went wrong with the command type!: {}", e),
+            },
+            query_content: parts[1].to_string(),
+        };
+        commands_and_contents.push(ccentry);
     }
-    if accepted {
-        Ok(commands_and_contents)
-    }
-    else {
-        Err("There was an error parsing the commands!")
-    }
+    Ok(commands_and_contents)
 }
 
 fn commandtype(cmd_str: &str) -> Result<CommandType, &'static str> {
-    let command_term:String;
-    match LIST_OF_COMMANDS.binary_search(&cmd_str) {
-        Ok(cmd) => command_term = LIST_OF_COMMANDS[cmd].to_string(),
-        Err(_) => command_term = "".to_string(),
+    let command_term = match LIST_OF_COMMANDS.binary_search(&cmd_str) {
+        Ok(cmd) => LIST_OF_COMMANDS[cmd].to_string(),
+        Err(_) => "".to_string(),
+    };
+    if command_term=="commit" {
+        Ok(CommandType::C0)
+    } else if command_term=="issue" {
+        Ok(CommandType::C1)
+    } else if command_term == "milestone" {
+        Ok(CommandType::C2)
+    } else {
+        Err("You have used an invalid command!")
     }
-    match command_term.as_str() {
-        "commit" => return Ok(CommandType::C0),
-        "issue" => return Ok(CommandType::C1),
-        "milestone" => return Ok(CommandType::C2),
-        _ => return Err("You have used an invalid command!"),
+}
 
+// temp func
+
+fn print_out(cmds: &Vec<Command>) {
+    let iterator = cmds.iter();
+    for i in iterator {
+        println!("Command action: {:?}", i.command_type);
+        println!("Query/Content {}", i.query_content);
     }
 }
