b20339e9c7dca08edae8b7f513c97ea1db7f02fb
William Sutton <psyws4@nottingham.ac.uk>
2023-01-13 23:46:46 +0000

    Documentation - Fully documented the read.rs file
    All done with general functionality here, will make a patch to increase efficiency alongside command creation.

--- src/read.rs
+++ src/read.rs
@@ -3,22 +3,33 @@ use std::fs;
 use std::io;
 use std::io::Write;
 use std::process;
-use std::sync::Arc;
 
+/// This will be the expected amount of arguments of the cl interpreter,
+/// it will essentially know when to exit the iterator/loop when commands are being interpreted.
 const EXPECTED_LENGTH_OF_ARGS:u8 = 2;
+/// This is our list of approved commands, at the minute, only has commit issue and milestone and will
+/// be used as a way of checking the sanity for what the user has input.
 const LIST_OF_COMMANDS:&'static [&str] = &["commit", "issue", "milestone"];
 
 #[derive(Debug)]
+/// This is our custom struct/type that contains two strings, currently the command_type and query_content
+/// we can use this struct for extensibility purposes if there was ever more information that needed to be known.
 pub struct CardCommand {
     command_type: String,
     query_content: String
 }
-
+/// This config struct will be a simple way (currently) to store information about our read configuration,
+/// this currently only contains the locatation + name of our file.
 pub struct Config {
     pub location: String
 }
-
 impl Config {
+    /// This function will be called on a Config objects, to construct the location and other key
+    /// information for later use so the filestream can be closed a lot quicker making the file more
+    /// available. (First port of call function)
+    /// # Arguments
+    /// `args` - The fs::Args type that will be everything the user puts into the command line when 
+    /// running the application.
     pub fn create(args: Args) -> Result<Config, &'static str> {
         let mut parsed: Vec<String> = Vec::new();
         let expected_length = EXPECTED_LENGTH_OF_ARGS;
@@ -33,9 +44,21 @@ impl Config {
         Err(error_msg)
     }
 }
+/// Simply opens the file and reads its contents to a string.
+/// # Arguments
+/// `path` - The string path that points to the the file to be opened.
 pub fn get_contents(path: &String) -> io::Result<String> {
     fs::read_to_string(path)
 }
+/// This function will split the read file contents depending on two specific conditions,
+/// 1. The line begins with "###" when special chars have been trimmed from the start
+/// 2. The line doesn't begin with "###".
+/// These two conditions will be important, as the string line will be further split
+/// if it begins with ###, once the contents of that string are split and have been added,
+/// the lines beginning with ### will be removed.
+/// # Arguments
+/// `content` - The String containing all of the contents of a file.
+/// `filename` - The perpetuated for use to identify which file the program needs to open to modify.
 pub fn split_by_comment(content: String, filename: &String) -> std::io::Result<()> {
     let mut cleansed:Vec<String> = Vec::new();
     let mut remove_card = fs::OpenOptions::new().write(true).truncate(true).open(filename)?;
@@ -52,15 +75,23 @@ pub fn split_by_comment(content: String, filename: &String) -> std::io::Result<(
             writeln!(remove_card, "{}", indv)?;
         }
     }
-    let x = commands(cleansed).unwrap_or_else(|e| {
+    let x = commands_conversion(cleansed).unwrap_or_else(|e| {
         println!("The command interpreter failed due to: {}", e);
         process::exit(1);
     });
     println!("{:?}", print_out(&x)); // TODO implement here
     Ok(())
 }
-
-fn commands(mut command_arr: Vec<String>) -> Result<Vec<CardCommand>, &'static str> {
+    /// This function is responsible for taking the String vector containing different actions and comments
+    /// and splitting them up into their organised contents for use in the git automation.
+    /// It will take lines beginning with @, remove the at and take the word next to it as a command arg for git.
+    /// If the line doesn't begin with an @ and there is no existing card commands inside vector, 
+    /// the line will be discarded (for now).
+    /// If the line doesn't begin with an @ and there is existing command in the vector, then the contents will be added
+    /// to the most recent vector. (How most docs work.)
+    /// # Arguments
+    /// `command_arr` - The array containing each line from the split_by_comment function.
+fn commands_conversion(mut command_arr: Vec<String>) -> Result<Vec<CardCommand>, &'static str> {
     let mut commands_and_contents:Vec<CardCommand> = Vec::new();
     for actionable_line in command_arr.iter_mut() {
         if actionable_line.starts_with("@") {
@@ -89,8 +120,7 @@ fn commands(mut command_arr: Vec<String>) -> Result<Vec<CardCommand>, &'static s
     Ok(commands_and_contents)
 }
 
-// temp func
-
+/// Temp test func.
 fn print_out(cmds: &Vec<CardCommand>) {
     for i in cmds.iter() {
         println!("CardCommand action: {:?}", i.command_type);
